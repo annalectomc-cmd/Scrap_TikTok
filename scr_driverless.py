945ee7fe4157974600c749dc1d25d4ab59845bd1
annalectomc-cmd <annalectomc@gmail.com>
2026-06-16 07:16:12 -0500

    comments navigation

--- scr_driverless.py
+++ scr_driverless.py
@@ -9,40 +9,58 @@ async def scrape_profile(user, max_videos=4):
 
         await driver.get(f"https://www.tiktok.com/@{user}", wait_load=True)
         await asyncio.sleep(random.uniform(5, 10))
+        # elem_search = await driver.find_element(By.CSS_SELECTOR, "div[data-e2e='nav-search']")
+        # await elem_search.click()
+        # await asyncio.sleep(random.uniform(2, 4))
+        # texto = "user"
+        # for letra in texto:
+        #     await elem_search.send_keys(letra)
+        #     await asyncio.sleep(random.uniform(0.05, 0.2))
+        
+
         elem = await driver.find_element(By.CSS_SELECTOR, "div[data-e2e='user-post-item']")
         await elem.click()
-        await asyncio.sleep(random.uniform(4, 9))
-        comments = []
+        await asyncio.sleep(random.uniform(5, 10))
+        comments = {}
 
         for i in range(max_videos):
 
-            elem_scroll = await driver.find_elements(By.CSS_SELECTOR, "div[data-comment-ui-enabled='true']")
             video_id = await driver.current_url
 
-            for x in range(3):
-                await driver.execute_script("arguments[0].scrollIntoView();", elem_scroll[-1])
-                await asyncio.sleep(random.uniform(3, 6))
+            for x in range(20):
 
-            items = await driver.find_elements(By.CSS_SELECTOR, "div[data-comment-ui-enabled='true']")
-
-            for item in items:           
-                try:
-                    
-                    usuario_el = await item.find_element(By.CSS_SELECTOR, "[data-e2e='comment-username-1']")
-                    texto_el = await item.find_element(By.CSS_SELECTOR, "[data-e2e='comment-level-1']")        
-                    comments.append({
-                        "user": await usuario_el.text,
-                        "comment": await texto_el.text,
-                        "video_id": video_id
-                    })
-                    await asyncio.sleep(random.uniform(1.5, 3))
-
-                except Exception:
-                    continue   # si falta algún campo, salta ese comentario
             
+                items = await driver.find_elements(By.CSS_SELECTOR, "div[data-comment-ui-enabled='true']")
+
+                for item in items:           
+                    try:
+                        
+                        cid = await item.get_attribute("id")
+
+                        if not cid or cid in comments:
+                            continue
+
+                        user_el = await item.find_element(By.CSS_SELECTOR, "[data-e2e='comment-username-1']")
+                        text_el = await item.find_element(By.CSS_SELECTOR, "[data-e2e='comment-level-1']")        
+                        comments[cid] = {
+                        "user": await user_el.text,
+                        "comment": await text_el.text,
+                        "video_id": video_id
+                        }
+
+                        await asyncio.sleep(random.uniform(1.5, 3))
+
+                    except Exception:
+                        continue   # si falta algún campo, salta ese comentario
+                
+                elem_scroll = await driver.find_elements(By.CSS_SELECTOR, "div[data-comment-ui-enabled='true']")
+                for x in range(10):
+                    await driver.execute_script("arguments[0].scrollIntoView();", elem_scroll[-1])
+                    await asyncio.sleep(random.uniform(3, 6))
+
             elem_com = await driver.find_element(By.CSS_SELECTOR, "button[data-e2e='arrow-right']")
             await elem_com.click()
-            await asyncio.sleep(random.uniform(10, 20))
+            await asyncio.sleep(random.uniform(5, 10))
         # async def on_response(params):
         #     try:
         #         resp = params.get("response", {})
@@ -62,4 +80,5 @@ async def scrape_profile(user, max_videos=4):
         # # activar red y suscribir el handler
         # await driver.execute_cdp_cmd("Network.enable", {})
         # driver.add_cdp_listener("Network.responseReceived", on_response)
-        return comments
+        
+        return list(comments.values())
